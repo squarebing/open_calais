f355dacfef29a54bf80b352259d3f593b6517ae7
Bing Hu <squarebing@gmail.com>
2015-04-27 06:47:15 +0000

    add rescue

--- lib/open_calais/response.rb
+++ lib/open_calais/response.rb
@@ -35,48 +35,49 @@ module OpenCalais
     end
 
     def parse(response)
-    begin
-      r = response.body
-      @language = r.doc.meta.language rescue nil
-      @language = nil if @language == 'InputTextTooShort'
+      begin
+        r = response.body
+        @language = r.doc.meta.language rescue nil
+        @language = nil if @language == 'InputTextTooShort'
 
-      r.each do |k,v|
-        case v._typeGroup
-        when 'topics'
-          self.topics << {:name => humanize_topic(v.categoryName), :score => v.score.to_f, :original => v.categoryName}
-        when 'socialTag'
-          self.tags << {:name => v.name.gsub('_', ' and ').downcase, :score => importance_to_score(v.importance)}
-        when 'entities'
-          item = {:guid => k, :name => v.name, :type => v._type.remove_formatting.titleize, :score => 1.0}
+        r.each do |k,v|
+          case v._typeGroup
+          when 'topics'
+            self.topics << {:name => humanize_topic(v.categoryName), :score => v.score.to_f, :original => v.categoryName}
+          when 'socialTag'
+            self.tags << {:name => v.name.gsub('_', ' and ').downcase, :score => importance_to_score(v.importance)}
+          when 'entities'
+            item = {:guid => k, :name => v.name, :type => v._type.remove_formatting.titleize, :score => 1.0}
 
-          instances = Array(v.instances).select{|i| i.exact.downcase != item[:name].downcase }
-          item[:matches] = instances if instances && instances.size > 0
+            instances = Array(v.instances).select{|i| i.exact.downcase != item[:name].downcase }
+            item[:matches] = instances if instances && instances.size > 0
 
-          if OpenCalais::GEO_TYPES.include?(v._type)
-            if (v.resolutions && v.resolutions.size > 0)
-              r = v.resolutions.first
-              item[:name]      = r.shortname || r.name
-              item[:latitude]  = r.latitude
-              item[:longitude] = r.longitude
-              item[:country]   = r.containedbycountry if r.containedbycountry
-              item[:state]     = r.containedbystate if r.containedbystate
+            if OpenCalais::GEO_TYPES.include?(v._type)
+              if (v.resolutions && v.resolutions.size > 0)
+                r = v.resolutions.first
+                item[:name]      = r.shortname || r.name
+                item[:latitude]  = r.latitude
+                item[:longitude] = r.longitude
+                item[:country]   = r.containedbycountry if r.containedbycountry
+                item[:state]     = r.containedbystate if r.containedbystate
+              end
+              self.locations << item
+            else
+              self.entities << item
             end
-            self.locations << item
-          else
-            self.entities << item
+          when 'relations'
+            item = v.reject{|k,v| k[0] == '_' || k == 'instances'} || {}
+            item[:type] = v._type.remove_formatting.titleize
+            self.relations << item
           end
-        when 'relations'
-          item = v.reject{|k,v| k[0] == '_' || k == 'instances'} || {}
-          item[:type] = v._type.remove_formatting.titleize
-          self.relations << item
         end
-      end
 
-      # remove social tags which are in the topics list already
-      topic_names = self.topics.collect{|topic| topic[:name].downcase}
-      self.tags.delete_if{|tag| topic_names.include?(tag[:name]) }
-    rescue Exception => e
-      Rails.logger.info "Parse Error: #{e.message}"
+        # remove social tags which are in the topics list already
+        topic_names = self.topics.collect{|topic| topic[:name].downcase}
+        self.tags.delete_if{|tag| topic_names.include?(tag[:name]) }
+      rescue Exception => e
+        Rails.logger.info "Parse Error: #{e.message}"
+      end
     end
   end
 end
